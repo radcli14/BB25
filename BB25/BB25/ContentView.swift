d1f08509c8bddf2057bf5e1805b7f0e6f83844a5
radcli14 <radcli14@gmail.com>
2025-07-03 15:58:27 +0200

    created a control overlay

--- BB25/BB25/ContentView.swift
+++ BB25/BB25/ContentView.swift
@@ -6,16 +6,60 @@
 //
 
 import SwiftUI
+import RealityKit
 
 struct ContentView: View {
+    
+    @State var fwd = false
+    @State var rev = false
+    @State var ccw = false
+    @State var cw = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        RealityView { content in
+            
+        }
+        .overlay(alignment: .bottom) {
+            controls
+        }
+    }
+    
+    /// The control overlay for forward, reverse, counter-clockwise, and clockwise motion
+    var controls: some View {
+        VStack(spacing: 12) {
+            Text("BB25")
+                .font(.title.weight(.black))
+            ZStack {
+                VStack(spacing: 36) {
+                    heldButton("FWD", systemImage: "arrow.up", isHeld: $fwd)
+                    heldButton("REV", systemImage: "arrow.down", isHeld: $rev)
+                }
+                HStack {
+                    heldButton("CCW", systemImage: "arrow.counterclockwise", isHeld: $ccw)
+                    Spacer()
+                    heldButton("CW", systemImage: "arrow.clockwise", isHeld: $cw)
+                }
+            }
+        }
+        .font(.headline)
+        .padding()
+        .background {
+            RoundedRectangle(cornerRadius: 12)
+                .fill(.ultraThinMaterial)
         }
         .padding()
+    }
+    
+    /// A button that activates a `isHeld` boolean on long press, that uses a conventional `title` and `systemImage` to form a label
+    func heldButton(_ title: String, systemImage: String, isHeld: Binding<Bool>) -> some View {
+        Label(title, systemImage: systemImage)
+            .foregroundColor(isHeld.wrappedValue ? .red : .blue)
+            .onLongPressGesture(
+                minimumDuration: 0.01) {
+                    
+                } onPressingChanged: { isPressed in
+                    isHeld.wrappedValue = isPressed
+                }
     }
 }
 
