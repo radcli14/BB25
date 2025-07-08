d220f1e04984c2d41cd0e749ca49b8404982f853
radcli14 <radcli14@gmail.com>
2025-07-08 16:08:20 +0200

    completely migrated logic into the view model

--- BB25/BB25/BB25RealityView+ViewModel.swift
+++ BB25/BB25/BB25RealityView+ViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import RealityKit
+import BB25_3D_Assets
 
 extension BB25RealityView {
     enum CameraType {
@@ -19,5 +20,55 @@ extension BB25RealityView {
         var requestReset = false
         var anchor: AnchorEntity?
         var controlState = JoyStick.ControlState()
+    }
+}
+
+extension BB25RealityView.ViewModel {
+    func resetButtonAction() {
+        requestReset = true
+    }
+    
+    func cameraButtonAction() {
+        camera = camera == .spatialTracking ? .virtual : .spatialTracking
+        requestReset = true
+    }
+    
+    // Creates a new anchor and adds the robot scene to it. Since the main body is async, there is a callback to send the anchor back to the RealityView content after it is prepared.
+    func resetScene(onComplete: @escaping (AnchorEntity) -> Void) {
+        DispatchQueue.main.async {
+            self.anchor?.removeFromParent()
+            
+            switch self.camera {
+            case .virtual:
+                self.anchor = AnchorEntity(world: .zero)
+                let perspectiveCamera = PerspectiveCamera()
+                perspectiveCamera.camera.fieldOfViewInDegrees = 69
+                perspectiveCamera.look(at: .init(-0.25, 0, 0.25), from: .init(0.75, 1, 1.25), relativeTo: nil)
+                self.anchor?.addChild(perspectiveCamera)
+            case .spatialTracking:
+                self.anchor = AnchorEntity(.plane(.horizontal, classification: .any,  minimumBounds: SIMD2<Float>(0.2, 0.2)))
+            }
+            
+            guard let anchor = self.anchor, let scene = try? Entity.load(named: "Scene", in: BB25_3D_Assets.bB25_3D_AssetsBundle) else {
+                self.requestReset = false
+                return
+            }
+                
+            scene.setParent(anchor)
+            scene.setScale(4 * .one, relativeTo: nil)
+            scene.setupPhysics()
+            
+            self.requestReset = false
+            
+            onComplete(anchor)
+        }
+    }
+
+    /// Sets the forces at each frame update
+    func applyForces() {
+        if controlState.isActive {
+            anchor?.chassis?.addForce(.init(controlState.rightForce, 0, 0), at: BoEBotProperties.rightWheelPosition, relativeTo: anchor?.chassis)
+            anchor?.chassis?.addForce(.init(controlState.leftForce, 0, 0), at: BoEBotProperties.leftWheelPosition, relativeTo: anchor?.chassis)
+        }
     }
 }
--- BB25/BB25/BB25RealityView.swift
+++ BB25/BB25/BB25RealityView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 import RealityKit
-import BB25_3D_Assets
 
 struct BB25RealityView: View {
     @State var viewModel = BB25RealityView.ViewModel()
@@ -22,8 +21,11 @@ struct BB25RealityView: View {
             case .virtual: content.camera = .virtual
             case .spatialTracking: content.camera = .spatialTracking
             }
+            viewModel.resetScene(onComplete: content.add) // Adds the anchor captured in the closure
             
-            resetScene(in: content)
+            let _ = content.subscribe(to: SceneEvents.Update.self, on: nil, componentType: nil) { event in
+                viewModel.applyForces() // Sets the forces at each frame update
+            }
         
         } update: { content in
             if viewModel.requestReset {
@@ -31,76 +33,27 @@ struct BB25RealityView: View {
                 case .virtual: content.camera = .virtual
                 case .spatialTracking: content.camera = .spatialTracking
                 }
-                resetScene(in: content)
+                viewModel.resetScene(onComplete: content.add)
             }
         }
         .realityViewCameraControls(.orbit)
         .navigationTitle("BB25")
         .toolbar {
-            ToolbarItem {
-                Button("Reset", systemImage: "arrow.counterclockwise") {
-                    viewModel.requestReset = true
-                }
-                .disabled(viewModel.requestReset)
+            ToolbarItemGroup(placement: .bottomBar) {
+                Button("Reset", systemImage: "arrow.counterclockwise", action: viewModel.resetButtonAction)
+                    .disabled(viewModel.requestReset)
+#if os(iOS)
+                Spacer()
+                Button("Camera", systemImage: "camera", action: viewModel.cameraButtonAction)
+                    .disabled(viewModel.requestReset)
+#endif
             }
         }
-        #if os(iOS)
-        .toolbar {
-            ToolbarItem {
-                Button("Camera", systemImage: "camera") {
-                    viewModel.camera = viewModel.camera == .spatialTracking ? .virtual : .spatialTracking
-                    viewModel.requestReset = true
-                }
-                .disabled(viewModel.requestReset)
-            }
-        }
-        #endif
         .overlay(alignment: .bottom) {
             JoyStick(controlState: $viewModel.controlState)
         }
         .ignoresSafeArea(.all)
     }
-    
-    // Creates a new anchor and adds the robot scene to it
-    func resetScene(in content: RealityViewCameraContent) {
-        DispatchQueue.main.async {
-            viewModel.anchor?.removeFromParent()
-            
-            switch viewModel.camera {
-            case .virtual:
-                viewModel.anchor = AnchorEntity(world: .zero)
-            case .spatialTracking:
-                viewModel.anchor = AnchorEntity(.plane(.horizontal, classification: .any,  minimumBounds: SIMD2<Float>(0.2, 0.2)))
-            }
-            
-            guard let anchor = viewModel.anchor, let scene = try? Entity.load(named: "Scene", in: BB25_3D_Assets.bB25_3D_AssetsBundle) else {
-                viewModel.requestReset = false
-                return
-            }
-                
-            scene.setParent(anchor)
-            scene.setScale(4 * .one, relativeTo: nil)
-            scene.setupPhysics()
-            
-            content.add(anchor)
-            
-            let _ = content.subscribe(to: SceneEvents.Update.self, on: nil, componentType: nil) { event in
-                applyForces(in: scene)
-            }
-            
-            viewModel.requestReset = false
-        }
-    }
-    
-    // MARK: - Controls
-    
-    func applyForces(in scene: Entity) {
-        if viewModel.controlState.isActive {
-            scene.chassis?.addForce(.init(viewModel.controlState.rightForce, 0, 0), at: BoEBotProperties.rightWheelPosition, relativeTo: scene.chassis)
-            scene.chassis?.addForce(.init(viewModel.controlState.leftForce, 0, 0), at: BoEBotProperties.leftWheelPosition, relativeTo: scene.chassis)
-        }
-    }
-    
 }
 
 #Preview {
